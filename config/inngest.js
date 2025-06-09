d3094a512e99944cb134432ea71786044c57219f
itsxifat <itsxifat22@gmail.com>
2025-06-09 21:17:32 +0600

    sec

--- config/inngest.js
+++ config/inngest.js
@@ -80,7 +80,7 @@ export const createUserOrder = inngest.createFunction(
     {
         id: "create-user-order",
         batchEvents:{
-            maxSize: 25,
+            maxSize: 5,
             timeout: 5, // seconds
         }
     },
